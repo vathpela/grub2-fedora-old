215a7ab68fd719ba08dfdd8cdc6fd1b4a571f33c
Peter Jones <pjones@redhat.com>
2014-09-16 15:20:57 -0500

    Traceback in grub_fatal.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grub-core/kern/misc.c
+++ grub-core/kern/misc.c
@@ -19,6 +19,7 @@
 
 #include <grub/misc.h>
 #include <grub/err.h>
+#include <grub/dl.h>
 #include <grub/mm.h>
 #include <stdarg.h>
 #include <grub/term.h>
@@ -1277,6 +1278,71 @@ abort (void)
 }
 #endif
 
+#ifndef GRUB_UTIL
+static void
+private_backtrace_print_address (void *addr)
+{
+  grub_dl_t mod;
+
+  FOR_DL_MODULES (mod)
+  {
+    grub_dl_segment_t segment;
+    for (segment = mod->segment; segment; segment = segment->next)
+      if (segment->addr <= addr && (grub_uint8_t *) segment->addr
+	  + segment->size > (grub_uint8_t *) addr)
+	{
+	  grub_printf ("%s.%x+%" PRIxGRUB_SIZE, mod->name, segment->section,
+		       (grub_size_t) ((grub_uint8_t *) addr - (grub_uint8_t *) segment->addr));
+	  return;
+	}
+  }
+
+  grub_printf ("%p", addr);
+}
+
+#define MAX_STACK_FRAME 102400
+#define ALIGN(addr, align) (addr + ((align - (addr % align)) % align))
+
+static int
+private_unwind_frame (unsigned long *fp, unsigned long *sp, unsigned long *pc)
+{
+  unsigned long high, low;
+  unsigned long newfp = *fp;
+
+  low = *sp;
+  high = ALIGN(low, MAX_STACK_FRAME);
+
+  if (newfp < low || newfp > high - 0x18 || newfp & 0xf)
+    return -1;
+
+  *sp = newfp + 0x10;
+  *fp = *(unsigned long *)newfp;
+  /* fp + 8 would be the pc when we return, but we want the caller instead */
+  *pc = *(unsigned long *)(newfp + 8) - 4;
+
+  return 0;
+}
+
+static void
+private_backtrace (void)
+{
+  register unsigned long current_sp asm("sp");
+  unsigned long fp = (unsigned long)__builtin_frame_address(0);
+  unsigned long pc = (unsigned long)private_backtrace;
+  unsigned long sp = current_sp;
+
+  while (1)
+    {
+      grub_printf ("sp: %p fp: %p pc: ", (void *)sp, (void *)fp);
+      private_backtrace_print_address ((void *)pc);
+      grub_printf ("\n");
+
+      if (private_unwind_frame (&fp, &sp, &pc) < 0)
+	break;
+    }
+}
+#endif
+
 void
 grub_fatal (const char *fmt, ...)
 {
@@ -1286,6 +1352,10 @@ grub_fatal (const char *fmt, ...)
   grub_vprintf (_(fmt), ap);
   va_end (ap);
 
+#ifndef GRUB_UTIL
+  grub_printf ("\n");
+  private_backtrace();
+#endif
   grub_abort ();
 }
 
